756c43b5013c3aeac217941609abe3de31e327da
Julius A. Odai <jlsodai@gmail.com>
2015-12-11 04:34:24 +0000

    truncate by word, regenerate images, further styling, minor changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,12 @@ class Recipe < ActiveRecord::Base
     has_many :comments
     belongs_to :dessert_type
     
-    has_attached_file :image, styles: { recipe_main: "900x470#", recipe_thumb: "350x250#", medium: "400x400#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, allow_nil: true
+    has_attached_file :image, 	styles: { recipe_main: "900x470#",
+    							recipe_thumb: "350x250#", medium: "400x400#",
+    							thumb: "100x100#" },
+    							default_url: ":style/missing.jpg"
+
+    validates_attachment_content_type :image,
+    							content_type: /\Aimage\/.*\Z/,
+    							allow_nil: true
 end

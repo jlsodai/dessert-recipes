0cb71bf49f232adb902fc9bea9cd7e9033381cf8
jlsodai <Jlsodai@gmail.com>
2015-12-11 08:27:18 +0000

    fix comment validation errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,13 @@
 class Comment < ActiveRecord::Base
     belongs_to :recipe
-     validates  :name,  presence: true 
+     validates  :name,  presence: true, 
     					 length: { maximum: 50}
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     
 
 
-    validates  :email, presence: true 
+    validates  :email, presence: true, 
     					 length: { maximum: 255 },
                          format: { with: VALID_EMAIL_REGEX },
                      uniqueness: { case_sensitive: false }
